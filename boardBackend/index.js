7a9f9a571d914a2dc30bf00d9f69f22fdebb42a5
hallamargret <hallamargret99@gmail.com>
2021-03-09 17:02:34 +0000

    almost done with patch

--- boardBackend/index.js
+++ boardBackend/index.js
@@ -69,7 +69,7 @@ app.get(apiPath + version + '/boards/:id', (req, res) =>{
 });
 
 
-//Create a new board
+//3.  Create a new board
 app.post(apiPath + version +'/boards' , (req, res) => {
     if (req.body === undefined ||
         req.body.name === undefined ||
@@ -95,7 +95,7 @@ app.post(apiPath + version +'/boards' , (req, res) => {
 
 
 
-// Update a board
+// 4.   Update a board
 
 app.put(apiPath + version +'/boards/:id', (req, res) => {
     if (req.body === undefined || req.body.name == undefined || req.body.description === undefined) 
@@ -122,11 +122,13 @@ app.put(apiPath + version +'/boards/:id', (req, res) => {
             return res.status(200).json(boards[i]);
         }
     }
-    return res.status(404).json({"message": "Board with id " + req.params.id + " does not exist."})
+    return res.status(404).json({"message": "Board with id " + req.params.id + " does not exist."});
 });
 
 
-// Delete a board
+
+
+// 5.  Delete a board
 app.delete(apiPath + version +'/boards/:id', (req, res) => {
     if (isNaN(req.params.id)) {
         return res.status(400).json({"message": "Error, id needs to be a number."});
@@ -148,6 +150,262 @@ app.delete(apiPath + version +'/boards/:id', (req, res) => {
 
     return res.status(404).json({"message": "Board with id " + req.params.id + " does not exist."})
 });
+
+
+
+
+// 6.  Delete all boards
+app.delete(apiPath + version + '/boards', (req, res) => {
+    var returnBoardsArray = boards.splice(0, boards.length);
+    //boards = [];
+
+    for (let i = 0; i < returnBoardsArray.length; i++){
+        //let taskIds = returnBoardsArray[i].tasks.slice();
+        returnBoardsArray[i].tasks = [];
+
+        for (let j = tasks.length - 1; j >= 0; j--){
+            //if (taskIds.includes(task[j].id)) {
+                if (tasks[j].boardId == returnBoardsArray[i].id) {
+                returnBoardsArray[i].tasks.push(tasks.splice(j,1));
+            }
+        }
+    }
+    console.log(returnBoardsArray);
+    return res.status(200).json(returnBoardsArray);
+});
+
+
+
+
+// Tasks
+
+
+
+function cmpByID(id1, id2){
+    return Number(id1) - Number(id2);
+
+}
+
+function cmpBydateCreated(date1, date2){
+    return (date1 - date2);
+
+}
+
+function cmpByTaskName(tName1, tName2){
+    //if (tName1 < tName2)
+    return (tName1 < tName2);
+}
+
+
+// Endpoints for tasks
+
+
+// 1. Read all tasks for a specified board
+// GET /api/v1/boards/:id/tasks
+
+app.get(apiPath + version +'/boards/:id/tasks', (req, res) => {
+    if (isNaN(req.params.id)) {
+        return res.status(400).json({"message": "Error, id needs to be a number."});
+    }
+
+    for (let i=0; i < boards.length; i++){
+        if (boards[i].id == req.params.id){
+            break;
+        }
+        if (i == boards.length && req.params.id != boards[i].id) {
+            return res.status(404).json({"message": "Board with id " + req.params.id + " does not exist."});
+        }   
+    }
+
+    let taskArray = [];
+
+    for (let i = 0; i < tasks.length; i++){
+        if (tasks[i].boardId == req.params.id){
+            taskArray.push({id: tasks[i].id, boardId: tasks[i].boardId, taskName: tasks[i].taskName, dateCreated:tasks[i].dateCreated, archived: tasks[i].archived});
+        }
+    }
+
+    if (taskArray.length > 1){
+        if (req.query.sort ==="taskName") {
+            taskArray = taskArray.sort(cmpByTaskName);
+        }
+        if (req.query.sort === "dateCreated") {
+            taskArray = taskArray.sort(cmpBydateCreated);
+        }
+        if (req.query.sort === "id") {
+            taskArray = taskArray.sort(cmpByID);
+        }
+        if (req.query.sort !="taskName" && req.query.sort !="dateCreated" && req.query.sort !="id") {
+            taskArray = taskArray.sort(cmpByID);
+        }
+
+    }
+    
+
+    //console.log(req.query.sort);
+    return res.status(200).json(taskArray);
+})
+
+
+
+// 2. Read an individaul task for the selected board
+// GET /api/v1/boards/:id/tasks/:taskId
+
+app.get(apiPath + version + '/boards/:id/tasks/:taskId', (req, res) =>{
+    
+    if (isNaN(req.params.id)) {
+        return res.status(400).json({"message": "Error. Board id needs to be a number."});
+    } 
+
+    if (isNaN(req.params.taskId)) {
+        return res.status(400).json({"message": "Error. task id needs to be a number."});
+    } 
+
+    for (let j = 0; j < boards.length ; j++){
+        if (boards[j].id == req.params.id){
+
+            for (let i = 0; i < tasks.length; i++) {
+                if (tasks[i].id == req.params.taskId && tasks[i].boardId == req.params.id){
+                    return res.status(200).json(tasks[i]);
+                }
+            }
+        }
+    }
+    
+    return res.status(404).json({"message": "Board with id " + req.params.id + " and task id " + req.params.taskId + " does not exist."});
+});
+
+
+// 3. Create a new task for a specified board
+ // POST /api/v1/boards/:id/tasks
+
+ app.post(apiPath + version +'/boards/:id/tasks' , (req, res) => {
+    if (req.body === undefined || req.body.taskName === undefined) {
+            return res.status(400).json({"message": "Error, task name is required in the request body."});
+        }
+
+    if (isNaN(req.params.id)) {
+        return res.status(400).json({"message": "Error. Board id needs to be a number."});
+    } 
+    
+    if (req.body.taskName === ""){
+        return res.status(400).json({"message": "name of the task may not be empty"})
+    }
+    
+    for (let i=0; i < boards.length; i++){
+        if (req.params.id == boards[i].id){
+            var myNewTask = {id: nextTaskId, boardId: req.params.id, taskName: req.body.taskName, dateCreated: Date.now(), archived: false};
+            // need to add the date to the new task, need to figure out how to make the date
+            
+            boards[i].tasks.push(myNewTask.id);
+            tasks.push(myNewTask);
+
+            nextTaskId = nextTaskId + 1;
+
+            return res.status(201).json(myNewTask);
+        }
+    }
+
+    return res.status(404).json({"message": "Board with id " + req.params.id + " does not exist."});
+
+});
+
+//{ id: '3', boardId: '3', taskName: "Prepare assignment 2", dateCreated: new Date(Date.UTC(2021, 00, 10, 16, 00)), archived: true }
+
+
+// 4. Delete a task for a specified board
+// DELETE  /api/v1/boards/:id/tasks/:taskID
+
+app.delete(apiPath + version +'/boards/:id/tasks/:taskID', (req, res) => {
+    if (isNaN(req.params.id)) {
+        return res.status(400).json({"message": "Error, id needs to be a number."});
+    }
+
+    if (isNaN(req.params.taskID)) {
+        return res.status(400).json({"message": "Error, task id needs to be a number."});
+    }
+
+    for (let j = 0; j < boards.length; j++){
+        if (boards[j].id == req.params.id) {
+            if (boards[j].tasks.includes(req.params.taskID)) {
+                for (let i = 0; i < tasks.length; i++){
+                    if (tasks[i].id == req.params.taskID){
+                        return res.status(200).json(tasks.splice(i, 1));
+                    }
+                
+                }
+                if (i == tasks.length) {
+                    return res.status(404).json({"message": "Board with id " + req.params.id + " does not have task with id " + req.params.taskID + ", it does not exist."})
+                }
+            }
+
+            else {
+                return res.status(404).json({"message": "Board with id " + req.params.id + " does not have task with id " + req.params.taskID + "."})
+            }
+            
+        }
+        if (j == boards.length) {
+            return res.status(404).json({"message": "Board with id " + req.params.id + " does not exist."})
+        }
+    }
+
+    return res.status(404).json({"message": "Board with id " + req.params.id + " and task id " + req.params.taskID + " does not exist."})
+});
+
+
+// 5. Partially update a task for a board
+// PATCH /api/v1/boards/:id/tasks/:taskid
+
+app.patch(apiPath + version + '/boards/:id/tasks/:taskID', (req, res) => {
+    if (isNaN(req.params.id)) {
+        return res.status(400).json({"message": "Error, id needs to be a number."});
+    }
+
+    if (isNaN(req.params.taskID)) {
+        return res.status(400).json({"message": "Error, task id needs to be a number."});
+    }
+    if (req.body === undefined || (req.body.taskName == undefined && req.body.archived == undefined
+        && req.body.boardId == undefined )){
+            return res.status(400).json({"message": "There has to be at least a taskName, archived or boardId in the request body."})
+        }
+
+
+    for (let j = 0; j < boards.length; j++){
+        if (boards[j].id == req.params.id) {
+            if (boards[j].tasks.includes(req.params.taskID)) {
+                for (let i = 0; i < tasks.length; i++){
+                    if (tasks[i].id == req.params.taskID){
+                        if (req.body.taskName !== undefined){
+                            tasks[i].taskName = req.body.taskName;
+                        }
+                        if (req.body.archived !== undefined){
+                            tasks[i].archived = req.body.archived;
+                        }
+                        if (req.body.boardId !== undefined){
+                            //remove the task from the old board
+                            var oldBoardId = tasks[i].boardId;
+                            for (let i=0; i < boards.length; i++){
+                                if (boards[i].id == oldBoardId){
+                                    boards[i].tasks.splice(i, 1);
+                                }
+                            }
+                            // add the task to the new board
+                            tasks[i].boardId = req.body.boardId;
+                            boards[req.body.boardId].tasks.push(tasks[i].id);
+                        }
+
+
+
+                        //return res.status(200).json(tasks.splice(i, 1));
+                    }
+                
+                }
+            }
+        }
+    }
+
+});
+
 
 
 
